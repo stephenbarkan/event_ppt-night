b67f0393957e69fa844bc133733c5cc1a5835b81
honeybody <honeybody@protonmail.com>
2026-09-11 14:39:07 -0500

    finished with favicon and og-image!

--- _data/global.js
+++ _data/global.js
@@ -3,7 +3,7 @@ export default {
   siteTitle: "RSVP to Frenz Group PowerPoint Night",
   siteDescription: "October 17th, 6pm at Solar Intentions",
   siteImage: "/assets/uploads/og-image.jpg",
-  favicon: "/assets/uploads/favicon.png",
-  themeColor: "#F4B9D4",
+  favicon: "/assets/uploads/favicon.svg",
+  themeColor: "color(display-p3 0.552941 0.203922 0.278431 / 1)",
   eventGraphic: "/assets/uploads/ppnight.gif",
 };
