74c316d678dc1035caa91444de3a7bef9022fc54
honeybody <honeybody@protonmail.com>
2026-09-11 14:45:14 -0500

    update it without slash

--- _data/global.js
+++ _data/global.js
@@ -1,5 +1,5 @@
 export default {
-  siteDomain: "https://frenz-group-ppt-night.netlify.app/",
+  siteDomain: "https://frenz-group-ppt-night.netlify.app",
   siteTitle: "RSVP to Frenz Group PowerPoint Night",
   siteDescription: "October 17th, 6pm at Solar Intentions",
   siteImage: "/assets/uploads/og-image.jpg",
