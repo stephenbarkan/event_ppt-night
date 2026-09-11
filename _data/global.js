342d6657e230781aba87a1f38a75b09b5a6c728a
honeybody <honeybody@protonmail.com>
2026-09-11 14:45:00 -0500

    update url

--- _data/global.js
+++ _data/global.js
@@ -1,5 +1,5 @@
 export default {
-  siteDomain: "localhost:8080",
+  siteDomain: "https://frenz-group-ppt-night.netlify.app/",
   siteTitle: "RSVP to Frenz Group PowerPoint Night",
   siteDescription: "October 17th, 6pm at Solar Intentions",
   siteImage: "/assets/uploads/og-image.jpg",
